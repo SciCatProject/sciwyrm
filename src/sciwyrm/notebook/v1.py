4b50a22cf0e97793bd0259ddee991aa8d9e2cd24
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-10-13 14:14:48 +0200

    Insert cells into notebook

--- src/sciwyrm/notebook/v1.py
+++ src/sciwyrm/notebook/v1.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2023 SciCat Project (https://github.com/SciCatProject/scitacean)
 """Version 1 notebooks."""
 
+from copy import deepcopy
 from typing import Literal
 
 from pydantic import BaseModel
@@ -17,8 +18,21 @@ class NotebookSpecV1(BaseModel):
 
     version: Literal["1"] = "1"
     dataset_pids: list[str]
+    scicat_url: str
+
+
+def _pids_cell_source(pids: list[str]) -> list[str]:
+    return ["input_dataset_pids = [\n", *(f'    "{pid}"\n' for pid in pids), "]"]
+
+
+def _scicat_url_cell_source(scicat_url: str) -> list[str]:
+    return [f'scicat_url = "{scicat_url}"']
 
 
 def format_notebook(spec: NotebookSpecV1) -> Notebook:
     """Return a formatted version 1 notebook."""
-    return _notebook_template_v1
+    nb = deepcopy(_notebook_template_v1)
+    cells = nb["cells"]
+    cells[1]["source"] = _scicat_url_cell_source(spec.scicat_url)
+    cells[2]["source"] = _pids_cell_source(spec.dataset_pids)
+    return nb
