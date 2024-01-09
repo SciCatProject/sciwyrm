1f54a9f56822ec1b61dfae77c47f7404923d35b9
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-01-09 14:42:50 +0100

    Update copyright year

--- src/sciwyrm/notebook/v1.py
+++ src/sciwyrm/notebook/v1.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright (c) 2023 SciCat Project (https://github.com/SciCatProject/sciwyrm)
+# Copyright (c) 2024 SciCat Project (https://github.com/SciCatProject/sciwyrm)
 """Version 1 notebooks."""
 
 from copy import deepcopy
