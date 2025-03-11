bf8dcbe213ca1e5ea5abff8671746e260955a28a
Max Novelli <max.novelli@ess.eu>
2025-03-11 10:36:07 +0100

    Added alive endpoint and renamed templates one

--- src/sciwyrm/main.py
+++ src/sciwyrm/main.py
@@ -5,6 +5,7 @@
 import json
 from typing import Annotated
 
+import uvicorn
 from fastapi import Depends, FastAPI, Request, Response
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
@@ -19,13 +20,28 @@ from .templates import (
     notebook_template_path,
 )
 
+import logging
+
 app = FastAPI()
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
 
-@app.get("/notebook/templates", response_description="Available templates")
+
+@app.get("/alive", response_description="The service is alive")
+async def list_templates(
+    config: Annotated[AppConfig, Depends(app_config)]
+) -> bool:
+    """Return a list of available notebook templates."""
+    return True
+
+
+@app.get("/templates", response_description="Available templates")
 async def list_templates(
     config: Annotated[AppConfig, Depends(app_config)]
 ) -> list[notebook.TemplateSummary]:
+    logger.info("Templates")
     """Return a list of available notebook templates."""
     return notebook.available_templates(config)
 
@@ -56,11 +72,12 @@ async def template_schema(
 
 
 @app.post("/notebook", response_model=dict, response_description="Rendered notebook")
-async def format_notebook(
+async def format_notebook_from_json(
     request: Request,
     templates: Annotated[Jinja2Templates, Depends(get_templates)],
     spec: notebook.NotebookSpecWithConfig = Depends(_inject_app_config),  # noqa: B008
 ) -> Response:
+
     """Format and return a notebook."""
     formatted = templates.TemplateResponse(
         name=notebook_template_path(spec.template_id),
@@ -70,3 +87,7 @@ async def format_notebook(
     nb = json.loads(formatted.body)
     notebook.insert_notebook_metadata(nb, spec)
     return JSONResponse(nb)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
